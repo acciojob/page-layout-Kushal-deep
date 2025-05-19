b67ab580b769659e35cf9c7ea3ef0ed1b949cc80
Sahebzad Kushal Deep <kushaldeep448@gmail.com>
2025-05-19 18:33:31 +0530

    completed-page-layout

--- src/components/App.js
+++ src/components/App.js
@@ -6,6 +6,9 @@ const App = () => {
   return (
     <div>
         {/* Do not remove the main div */}
+        <header>Welcome to my Website</header>
+        <div>This is the content of my website</div>
+        <footer>@2023 mywebsite.All rights Reserved</footer>
     </div>
   )
 }
