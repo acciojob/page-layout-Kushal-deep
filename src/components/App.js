f9bfe7403640af2c26cea39dca6d7bd118a14f4a
Sahebzad Kushal Deep <kushaldeep448@gmail.com>
2025-05-19 19:17:04 +0530

    completed page-layout assignment

--- src/components/App.js
+++ src/components/App.js
@@ -11,7 +11,7 @@ const App = () => {
         {/* Do not remove the main div */}
         <PageLayout 
         header={<header>Welcome to my website</header>}
-        footer={<footer>@2023 mywebsite. All rights Reserved</footer>}
+        footer={<footer>&copy; 2023 My Website.All rights reserved.</footer>}
         >
           <div>This is the content of my website</div>
 
